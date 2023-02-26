f5d7d0d5596e4329e9a6bf70a2bd879038d00679
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-02-26 10:38:18 -0500

    Update create.sql

--- SQLtemplates/create.sql
+++ SQLtemplates/create.sql
@@ -10,10 +10,10 @@
 -------------------------
 CREATE TABLE TeamStanding
 (
-  standing_id	int	    NOT NULL UNIQUE ,
-  team_id    	int  	NOT NULL ,
-  league_id  	char(5) NOT NULL ,
-  season_id	    int	    NULL
+  standing_id	  int	      NOT NULL UNIQUE ,
+  team_id    	  int  	    NOT NULL ,
+  league_id  	  char(5)   NOT NULL ,
+  season_id	    int	      NULL
 );
 
 --------------------------
@@ -24,11 +24,11 @@ CREATE TABLE TeamStandingStat
   stat_id 	    int	    NOT NULL UNIQUE ,
   standing_id   int     NOT NULL ,
   games_played	int	    NULL ,
-  wins    	    int  	NULL ,
+  wins    	    int  	  NULL ,
   draws   	    int   	NULL ,
-  losses     	int  	NULL ,
+  losses     	  int  	  NULL ,
   goals_scored 	int	    NULL ,
-  goals_lost 	int  	NULL ,
+  goals_lost 	  int  	  NULL ,
   goal_diff   	int	    NULL 
 );
 
@@ -39,11 +39,11 @@ CREATE TABLE LeagueHistory
 (
   season_id	        int	            NOT NULL UNIQUE,
   league_id	        char(5)	        NOT NULL ,
-  winning_team_id	int	            NULL ,
-  season_year	    int	            NOT NULL ,
+  winning_team_id	  int	            NULL ,
+  season_year	      int	            NOT NULL ,
   revenue           decimal(12, 2)  NULL ,
   viewership        int             NULL ,
-  mvp_id	        int	            NULL  
+  mvp_id	          int	            NULL  
 );
 
 --------------------------
@@ -51,11 +51,11 @@ CREATE TABLE LeagueHistory
 --------------------------
 CREATE TABLE League
 (
-  year_founded  	int          	NULL ,
+  year_founded  	  int          	NULL ,
   league_name 	    char(50)    	NOT NULL ,
-  league_id    	    char(5) 		NOT NULL UNIQUE ,
+  league_id    	    char(5) 		  NOT NULL UNIQUE ,
   num_teams   	    int          	NULL ,
-  recurrence 	    char(50) 	    NULL 
+  recurrence 	      char(50) 	    NULL 
 );
 
 
@@ -65,12 +65,12 @@ CREATE TABLE League
 CREATE TABLE Match
 (
   match_id	    int	        NOT NULL UNIQUE ,
-  date_played	date	    NULL ,
+  date_played	  date	      NULL ,
   league_id	    char(5)	    NULL ,
-  team1_id  	int     	NOT NULL ,
-  team2_id 	    int     	NOT NULL ,
-  goals_team1   int      	NULL ,
-  goals_team2	int	        NULL ,
+  team1_id  	  int     	  NOT NULL ,
+  team2_id 	    int     	  NOT NULL ,
+  goals_team1   int      	  NULL ,
+  goals_team2	  int	        NULL ,
   outcome       int         NOT NULL -- the outcome is a 1 when team 1 wins, a 2 when team 2 wins, and a 0 when it is a draw
 );
 
@@ -80,11 +80,11 @@ CREATE TABLE Match
 CREATE TABLE Player
 (
   first_name   	    char(10)      	NULL ,
-  last_name	        char(20)		NOT NULL ,
-  middle_initial    char(1)		    NULL ,
-  player_id    	    int      	    NOT NULL UNIQUE ,
+  last_name	        char(20)		    NOT NULL ,
+  middle_initial    char(1)		      NULL ,
+  player_id    	    int      	      NOT NULL UNIQUE ,
   year_salary  	    decimal(12,2)   NULL ,
-  field_position	char(20)		NULL ,
+  field_position	  char(20)		    NULL ,
   team_id 	        int	  	        NULL 
 );
 
@@ -93,12 +93,12 @@ CREATE TABLE Player
 -----------------------
 CREATE TABLE PlayerStat
 (
-  stat_id	    int 	NOT NULL UNIQUE ,
+  stat_id	      int 	  NOT NULL UNIQUE ,
   player_id     int     NOT NULL ,
-  games_played	int		NULL ,
-  goals  	    int	 	NULL ,
-  assists	    int		NULL ,
-  hat_tricks	int		NULL 	
+  games_played	int		  NULL ,
+  goals  	      int	 	  NULL ,
+  assists	      int		  NULL ,
+  hat_tricks	  int		  NULL 	
 );
 
 -----------------------
@@ -116,12 +116,12 @@ CREATE TABLE PlayerAward
 -----------------------
 CREATE TABLE Team
 (
-  team_id      	    int 		NOT NULL UNIQUE ,
-  team_name    	    char(50) 	NOT NULL ,
-  year_founded 	    int	 	    NULL ,
-  league_id    	    char(5) 	NOT NULL ,
-  games_played  	int	  	    NULL ,
-  wins    	        int	 	    NULL ,
+  team_id      	    int 		    NOT NULL UNIQUE ,
+  team_name    	    char(50) 	  NOT NULL ,
+  year_founded 	    int	 	      NULL ,
+  league_id    	    char(5) 	  NOT NULL ,
+  games_played  	  int	  	    NULL ,
+  wins    	        int	 	      NULL ,
   country 	        char(50)    NULL 
 );
 
@@ -130,11 +130,11 @@ CREATE TABLE Team
 ----------------------
 CREATE TABLE Award
 (
-  award_id	    int		        NOT NULL UNIQUE ,
-  award_name	char(50)		NOT NULL ,
-  year_created	int		        NULL ,
-  times_given	int		        NULL ,
-  award_desc	varchar(500)	NULL 
+  award_id	    int		          NOT NULL UNIQUE ,
+  award_name	  char(50)		    NOT NULL ,
+  year_created	int		          NULL ,
+  times_given	  int		          NULL ,
+  award_desc	  varchar(500)	  NULL 
 );
 
 ----------------------
