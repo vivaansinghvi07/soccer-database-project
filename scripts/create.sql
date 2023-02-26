87be0786b16349e337b99da8e0eb32c8f60a9766
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-02-26 11:07:23 -0500

    testing formatting

--- scripts/create.sql
+++ scripts/create.sql
@@ -10,10 +10,10 @@
 -------------------------
 CREATE TABLE TeamStanding
 (
-  standing_id	  int	      NOT NULL UNIQUE ,
-  team_id    	  int  	    NOT NULL ,
-  league_id  	  char(5)   NOT NULL ,
-  season_id	    int	      NULL
+  standing_id	    int	        NOT NULL UNIQUE ,
+  team_id    	    int  	    NOT NULL ,
+  league_id  	    char(5)     NOT NULL ,
+  season_id         int	        NULL
 );
 
 --------------------------
