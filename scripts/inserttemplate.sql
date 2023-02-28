b5e8177f5485c509883ba95d2cdfd42d255e9fcb
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-02-28 10:45:04 -0500

    added semicolons

--- scripts/inserttemplate.sql
+++ scripts/inserttemplate.sql
@@ -3,20 +3,20 @@
 INSERT INTO League(year_founded, league_name, league_id, num_teams, recurrence) 
     VALUES ('', '', '', '', '');
 INSERT INTO Team(team_id, team_name, year_founded, league_id, games_played, wins, country) 
-    VALUES ('', '', '', '', '', '', '')
+    VALUES ('', '', '', '', '', '', '');
 INSERT INTO Player(first_name, last_name, middle_initial, player_id, year_salary, field_position, team_id)
-    VALUES ('', '', '', '', '', '', '')
+    VALUES ('', '', '', '', '', '', '');
 INSERT INTO Award(award_id, award_name, year_created, times_given, award_desc)
-    VALUES ('', '', '', '', '')
+    VALUES ('', '', '', '', '');
 INSERT INTO PlayerStat(stat_id, player_id, games_played, goals, assists, hat_tricks)
-    VALUES('', '', '', '', '', '')
+    VALUES('', '', '', '', '', '');
 INSERT INTO PlayerAward(playeraward_id, date_awarded, player_id, award_id)
-    VALUES('', '', '', '')
+    VALUES('', '', '', '');
 INSERT INTO LeagueHistory(season_)id, league_id, winning_team_id, season_year, revenue, viewership, mvp_id)
-    VALUES ('', '', '', '', '', '', '')
+    VALUES ('', '', '', '', '', '', '');
 INSERT INTO TeamStanding(standing_id, team_id, league_id, season_id)
-    VALUES('', '', '', '')
+    VALUES('', '', '', '');
 INSERT INTO TeamStandingStat(stat_id, standing_id, games_played, wins, draws, losses, goals_scored, goals_lost, goal_diff)
-    VALUES('', '', '', '', '', '', '', '', '')
+    VALUES('', '', '', '', '', '', '', '', '');
 INSERT INTO Match(match_id, date_played, league_id, team1_id, team2_id, goals_team1, goals_team2, outcome)
-    VALUES('', '', '', '', '', '', '', '')
+    VALUES('', '', '', '', '', '', '', '');
