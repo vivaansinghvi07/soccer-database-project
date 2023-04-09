3ad16073903745ec7507978e3c9072743f4c51a3
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-09 16:18:59 -0400

    created select function

---
+++ scripts/python/testing/select.py
@@ -0,0 +1,50 @@
+from database import Database
+
+# creates database
+db = Database()
+
+# neatifies multiple rows
+def format(ls):
+    output = ""
+    for row in ls:
+        output += str(row) + "\n"
+    return output
+        
+
+# select 5 teams from the teams table
+def selectTeams():
+    db.query("SELECT * FROM Team")
+    print(format(db.cursor.fetchmany(size=5)))
+
+# select 1 player
+def selectPlayer():
+    db.query("SELECT * FROM Player")
+    print(db.cursor.fetchone())
+
+# select all player-team relations
+def selectPlayerTeams():
+    db.query("SELECT p.first_name, p.last_name, t.team_name FROM Player p INNER JOIN PlayerTeam pt ON pt.player_id = p.player_id INNER JOIN Team t on t.team_id = pt.team_id")
+    print(format(db.cursor.fetchall()))
+
+# select all leagues
+def selectLeagues():
+    db.query("SELECT * FROM League")
+    print(format(db.cursor.fetchall()))
+
+if __name__ == "__main__":
+    print(
+        """
+        1. Select 5 teams
+        2. Select 1 player
+        3. Select all player-team relations
+        4. Select all leagues
+        """
+    )
+    num = input("What function do you want to execute? ")
+
+    if not num in ["1", "2", "3", "4"]:
+        print("Invalid choice!")
+    else:
+        num = int(num)
+        print()
+        eval(["selectTeams", "selectPlayer", "selectPlayerTeams", "selectLeagues"][num-1] + "()")
