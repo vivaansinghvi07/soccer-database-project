b4175d2a8716ae37eb5cc372e2fb277f21579efe
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-09 16:24:00 -0400

    fix formatting

--- scripts/python/testing/select.py
+++ scripts/python/testing/select.py
@@ -1,37 +1,38 @@
 from database import Database
 
-# creates database
-db = Database()
-
 # neatifies multiple rows
 def format(ls):
     output = ""
     for row in ls:
         output += str(row) + "\n"
-    return output
-        
+    return output     
 
 # select 5 teams from the teams table
-def selectTeams():
+def selectTeams(db):
     db.query("SELECT * FROM Team")
     print(format(db.cursor.fetchmany(size=5)))
 
 # select 1 player
-def selectPlayer():
+def selectPlayer(db):
     db.query("SELECT * FROM Player")
     print(db.cursor.fetchone())
 
 # select all player-team relations
-def selectPlayerTeams():
+def selectPlayerTeams(db):
     db.query("SELECT p.first_name, p.last_name, t.team_name FROM Player p INNER JOIN PlayerTeam pt ON pt.player_id = p.player_id INNER JOIN Team t on t.team_id = pt.team_id")
     print(format(db.cursor.fetchall()))
 
 # select all leagues
-def selectLeagues():
+def selectLeagues(db):
     db.query("SELECT * FROM League")
     print(format(db.cursor.fetchall()))
 
-if __name__ == "__main__":
+def main():
+
+    # creates database
+    db = Database()
+
+    # get type of function to run
     print(
         """
         1. Select 5 teams
@@ -47,4 +48,10 @@ if __name__ == "__main__":
     else:
         num = int(num)
         print()
-        eval(["selectTeams", "selectPlayer", "selectPlayerTeams", "selectLeagues"][num-1] + "()")
+        eval(["selectTeams", "selectPlayer", "selectPlayerTeams", "selectLeagues"][num-1] + "(db)")
+
+    # closes connection
+    db.end()
+
+if __name__ == "__main__":
+    main()
