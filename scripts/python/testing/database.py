d1e4c5166651380a82ca6a54f93ab0188c85eda7
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-04-16 10:43:01 -0400

    added config parser

--- scripts/python/testing/database.py
+++ scripts/python/testing/database.py
@@ -1,15 +1,20 @@
 import psycopg2
 import os
+from configparser import ConfigParser
 
 class Database: 
     def __init__(self):
-        self.connection = psycopg2.connect(
-            host="localhost",
-            database="soccer",
-            port=5432,
-            user="postgres",
-            password=os.getenv("SQLPASSWORD")
-        )
+
+        # obtain information from the creds.ini file
+        parser = ConfigParser()
+        parser.read('creds.ini')
+        pgcreds = {}
+        if parser.has_section('pgdb'):
+            items = parser.items("pgdb")
+            for item in items:
+                pgcreds[item[0]] = item[1]
+
+        self.connection = psycopg2.connect(**pgcreds)
         self.cursor = self.connection.cursor()
     
     def query(self, prompt):
